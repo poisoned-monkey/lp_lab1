593a7b1bb5bc1852bb5db39c2d7f1128e92f4cd0
Наталия Заводнова <nataliazavodnova@MacBook-Pro-Natalia.local>
2020-01-07 04:30:37 +0300

    last commt

--- task1.pl
+++ task1.pl
@@ -1,7 +1,44 @@
-% Первая часть задания - предикаты работы со списками
+% РџСЂРµРґРёРєР°С‚С‹ СЂР°Р±РѕС‚С‹ СЃРѕ СЃРїРёСЃРєР°РјРё
 
-% Ниже - пример кода, который вы можете удалить.
+my_length([],0).
+my_length([_|T], N) :-
+  my_length(T, N1),
+  N is N1 + 1.
 
-mylen([],0).
-mylen([X|T],N) :- mylen(T,N1), N is N1+1.
+my_member(X,[X|_]).
+my_member(X,[_|T]) :-
+  my_member(X,T).
 
+my_append([],X,X).
+my_append([X|Y],Z,[X|T]) :-
+  my_append(Y,Z,T).
+
+my_remove(X,[X|T],T).
+my_remove(X,[H|T],[H|Y]) :-
+  my_remove(X,T,Y).
+
+my_permute([],[]).
+my_permute(X,[H|T1]) :-
+  my_remove(H,X,T),
+  my_permute(T,T1).
+
+my_sublist(S,R) :-
+  my_append(X,_,S),
+  my_append(_,R,X).
+
+% РќР°С…РѕР¶РґРµРЅРёРµ СЌР»РµРјРµРЅС‚Р° СЃРїРёСЃРєР°, СЃР»РµРґСѓСЋС‰РµРіРѕ Р·Р° РґР°РЅРЅС‹Рј
+
+f([X,Y|_],X,R) :- 
+R is Y, !. 
+
+f([X|T], E, R) :- 
+f(T, E, R).
+
+% Р Р°Р·РґРµР»РµРЅРёРµ СЃРїРёСЃРєР° РЅР° РґРІР° РїРѕ РїСЂРёРЅС†РёРїСѓ С‡РµС‚РЅРѕСЃС‚Рё СЌР»РµРјРµРЅС‚РѕРІ
+
+f([],[],[]). 
+f([H|T], [H|R1], R2) :- 
+ischet(H), f(T,R1, R2). 
+
+f([H|T], R1, [H|R2]):- 
+not(ischet(H)), f(T, R1, R2).
